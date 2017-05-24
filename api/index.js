1fdd2ad3e47972bfb2dc43cd03a583ed7c505aa0
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-05-24 12:18:28 +1000

    Use alias

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
 module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://site-jyltvzxjuk.now.sh');
+  res.setHeader('Access-Control-Allow-Origin', 'https://upland-site.now.sh');
 
   const request = await fetch('https://api.github.com/orgs/zeit/members')
   const data = await request.json()
