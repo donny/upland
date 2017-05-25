893bb69b526d26dda20780a414e69a51c688b0e9
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-05-26 07:57:06 +1000

    Fetch from Reddit EarthPorn

--- api/index.js
+++ api/index.js
@@ -3,8 +3,17 @@ const fetch = require('node-fetch')
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://upland-site.now.sh');
 
-  const request = await fetch('https://api.github.com/orgs/zeit/members')
-  const data = await request.json()
+  const request = await fetch('https://www.reddit.com/r/earthporn.json')
+  const json = await request.json()
+  const data = json.data.children
 
-  return data
+  var results = []
+
+  data.forEach(child => {
+    if (child.data.thumbnail !== 'default') {
+      results.push(child.data.url)
+    }
+  })
+
+  return results
 }
